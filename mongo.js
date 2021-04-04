9d55b263486870a19a5bd7d7bb1311a4fb2b19cc
cvegac <cvegacristian@gmail.com>
2021-04-03 22:39:44 -0500

    1

---
+++ mongo.js
@@ -0,0 +1,68 @@
+var users = [
+    {
+        id: 1,
+        name: "user1",
+        email: "u1@u",
+        dashboard:[
+            {
+                type: number,
+                color: red,
+                varName: "temperature",
+                unit: "°C"
+            },
+            {
+                type: boolean,
+                color: red,
+                varName: "temperature",
+                unit: "°C"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "user2",
+        email: "u2@u"
+    }
+];
+
+var data = [
+    {
+        time: 31235468152,
+        var: "temp",
+        value: 23
+    },
+    {
+        time: 31235468152,
+        var: "temp",
+        value: 23
+    },
+    {
+        time: 31235468152,
+        var: "temp",
+        value: 23
+    }
+]
+
+var comments = [
+    {
+        id: 1213,
+        comment: "Hi!",
+        user_id: 1
+    },
+    {
+        id: 1113,
+        comment: "Bye!",
+        user_id: 1
+    },
+    {
+        id: 1214,
+        comment: "Bye bye!",
+        user_id: 2
+    }
+];
+
+var user = {
+    id: 1,
+    name: "user1",
+    email: "u@u"
+}
